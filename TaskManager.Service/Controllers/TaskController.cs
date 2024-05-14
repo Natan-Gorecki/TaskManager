21d854fe2b68fc867aa7d2bea3297c5235cafea2
Natan Górecki <97natan@gmail.com>
2024-05-15 01:59:17 +0200

    Configured application to map controllers

---
+++ TaskManager.Service/Controllers/TaskController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Service.Database;
+using TaskManager.Service.Database.Models;
+
+namespace TaskManager.Service.Controllers;
+
+[Route("tasks")]
+[ApiController]
+public class TaskController : ControllerBase
+{
+    private readonly TaskManagerContext _context;
+
+    public TaskController(TaskManagerContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult GetTasks()
+    {
+        var space = _context.Spaces.FirstOrDefault();
+
+        var singleEpic = _context.Tasks.FirstOrDefault(x => x.Id == "IT-001");
+
+        var epic = _context.Tasks.Where(x => x.Id == "IT-001")
+            .Include(x => x.ChildTaskRelations)
+                .ThenInclude(x => x.ChildTask)
+            .FirstOrDefault();
+
+        var firstTask = _context.Tasks.Where(x => x.Id == "IT-003")
+            .Include(x => x.ParentTaskRelations)
+                .ThenInclude(x => x.ParentTask)
+            .FirstOrDefault();
+
+        return Ok("DONE");
+    }
+}
--- TaskManager.Service/Program.cs
+++ TaskManager.Service/Program.cs
@@ -13,32 +13,11 @@ services.AddDbContext<TaskManagerContext, SqliteTaskManagerContext>();
 var app = builder.Build();
 DatabaseInitializer.Seed(app.Services);
 
-//IF app.Environment.IsDevelopment()
+// IF app.Environment.IsDevelopment()
 app.UseSwagger();
 app.UseSwaggerUI();
-
+// ENDIF
 //app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
-var summaries = new[]
-{
-"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = summaries[Random.Shared.Next(summaries.Length)]
-        })
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.MapControllers();
 app.Run();

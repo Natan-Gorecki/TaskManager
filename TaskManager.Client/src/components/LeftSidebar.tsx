d9f116c2e39b6b616e3b66a743760f9eb233e519
Natan Górecki <97natan@gmail.com>
2024-07-06 23:22:42 +0200

    feat: style left sidebar

--- TaskManager.Client/src/components/LeftSidebar.tsx
+++ TaskManager.Client/src/components/LeftSidebar.tsx
@@ -1,30 +1,49 @@
 import {
   Box,
+  Divider,
   Drawer,
   IconButton,
   List,
+  ListItem,
   ListItemButton,
+  ListItemIcon,
   ListItemText,
   Toolbar
 } from '@mui/material';
 import { useRouter } from 'next/navigation';
 
+import BoardIcon from '@mui/icons-material/ViewKanbanOutlined'
 import CloseIcon from '@mui/icons-material/Close'
+import HomeIcon from '@mui/icons-material/HomeOutlined'
+import TasksIcon from '@mui/icons-material/ListAltOutlined'
 
 interface LeftSidebarProps {
   open: boolean;
   onClose: () => void;
 }
 
-interface Route {
+interface MenuItem {
+  icon: React.ReactElement;
   name: string;
-  path: string;
+  route: string;
 }
 
-const menuRoutes: Route[] = [
-  { name: 'Home', path: '/' },
-  { name: 'Board', path: '/board' },
-  { name: 'Tasks', path: '/tasks' }
+const menuRoutes: MenuItem[] = [
+  {
+    icon: <HomeIcon/>,
+    name: 'Home',
+    route: '/'
+  },
+  {
+    icon: <BoardIcon/>,
+    name: 'Board',
+    route: '/board'
+  },
+  {
+    icon: <TasksIcon/>,
+    name: 'Tasks',
+    route: '/tasks'
+  }
 ];
 
 export default function LeftSidebar({ open, onClose }: LeftSidebarProps): React.ReactElement {
@@ -38,28 +57,36 @@ export default function LeftSidebar({ open, onClose }: LeftSidebarProps): React.
       sx={{
         '& .MuiDrawer-paper': {
           borderTopRightRadius: '8px',
-          borderBottomRightRadius: '8px'
+          borderBottomRightRadius: '8px',
+          width: '20rem'
         }
       }}
     >
       <Toolbar>
+        <Box sx={{flexGrow: 1}}/>
         <IconButton onClick={() => onClose()}>
           <CloseIcon/>
         </IconButton>
       </Toolbar>
       <Box >
         <List>
-          {menuRoutes.map((route: Route) => (
-            <ListItemButton
-              key={route.name}
-              onClick={() => {
-                router.push(route.path);
-                onClose();
-              }}
-            >
-              <ListItemText primary={route.name}/>
-            </ListItemButton>
+          {menuRoutes.map((menuItem: MenuItem) => (
+            <ListItem disablePadding>
+              <ListItemButton
+                key={menuItem.name}
+                onClick={() => {
+                  router.push(menuItem.route);
+                  onClose();
+                }}
+              >
+                <ListItemIcon>
+                  {menuItem.icon}
+                </ListItemIcon>
+                <ListItemText primary={menuItem.name}/>
+              </ListItemButton>
+            </ListItem>
           ))}
+          <Divider/>
         </List>
       </Box>
     </Drawer>

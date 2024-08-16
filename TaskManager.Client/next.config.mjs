ef19ebc1372b08ed695ef58dffd0327ee9ac3d4f
Natan Górecki <97natan@gmail.com>
2024-08-16 19:25:56 +0200

    style: explicit defined strict mode

--- TaskManager.Client/next.config.mjs
+++ TaskManager.Client/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // React's Strict Mode is a development mode only feature for highlighting potential problems in an application.
+  // It helps to identify unsafe lifecycles, legacy API usage, and a number of other features.
+  reactStrictMode: true
+};
 
 export default nextConfig;
